dbd93a2b039af48e89461499d040b17158dd66d1
VladikRebko <rebko.v.v@mail.com>
2018-10-04 20:04:00 +0300

    longest-consecutive-sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+
+	let currentCountTheConsecutiveElements = 1, bestCountTheConsecutiveElements = 1;
+
+	if(array.length == 0 ) return 0;
+
+	array.sort( (a, b) => {
+		return a - b;
+	});
+
+	for ( let i = 0; i < array.length - 1; i++ ){
+		if(array[i] == (array[i + 1])){ 
+			continue;
+		}
+
+		if( array[i] == (array[i + 1] - 1 )){
+			currentCountTheConsecutiveElements++;
+		}
+
+		if( bestCountTheConsecutiveElements < currentCountTheConsecutiveElements ){
+			bestCountTheConsecutiveElements = currentCountTheConsecutiveElements;
+		}
+
+		if( array[i] != (array[i + 1] - 1 )){
+			currentCountTheConsecutiveElements = 1;
+		}
+	}
+
+	return bestCountTheConsecutiveElements;
 }
